b9f33c268ca60b57de1f96c0a2ba3068119e95d8
TaiYun712 <wendy0712000@gmail.com>
2024-11-13 18:06:11 +0800

    草食動物們生命值會在3分鐘後歸零(UI實裝)

--- Assets/FC Scripts/GrassAnimal.cs
+++ Assets/FC Scripts/GrassAnimal.cs
@@ -1,20 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GrassAnimal : MonoBehaviour
 {
-    public float g_Alivetime;
-    public float g_Hungrytime;
+    public float  maxAlivetime,maxHungry;
+    float currentAlive, currentHungry;
 
+    public Slider aliveSlider, hurgrySlider;
 
     void Start()
     {
-        
+         currentAlive = maxAlivetime;
+         currentHungry= maxHungry ;
+
+        aliveSlider.maxValue = maxAlivetime;
+        aliveSlider.value = currentAlive;
+
+
     }
 
     void Update()
     {
-        
+        GrassAnimalAliveTime();
+    }
+
+    void GrassAnimalAliveTime()
+    {
+        if(currentAlive > 0)
+        {
+            currentAlive -= Time.deltaTime;
+            aliveSlider.value = currentAlive;
+
+        }
+
     }
 }

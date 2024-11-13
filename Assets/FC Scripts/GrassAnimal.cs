ca1d0ebc9804ea85d84777b4af8a977190e786a5
TaiYun712 <wendy0712000@gmail.com>
2024-11-14 05:45:19 +0800

    會吃草了但要吃不吃的，還會停住

--- Assets/FC Scripts/GrassAnimal.cs
+++ Assets/FC Scripts/GrassAnimal.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.UI;
 
 public class GrassAnimal : MonoBehaviour
@@ -9,11 +10,20 @@ public class GrassAnimal : MonoBehaviour
     public float currentAlive, currentHungry;
 
     float hungryTime; //低於此值開始覓食
+    public float searchRadius; //覓食範圍
+    GameObject targetGrass; //目標草
 
     public Slider aliveSlider, hurgrySlider;
 
+    NavMeshAgent agent;
+    public bool isMoving = false;
+    public float moveRadius; //隨機移動範圍
+    public float waitTime; //隨機移動間隔
+    float timer; //下次移動時間
+
     private void Awake()
     {
+        agent = GetComponent<NavMeshAgent>();
         RandomLive();
         RandomHungry();
     }
@@ -36,9 +46,10 @@ public class GrassAnimal : MonoBehaviour
     {
         GrassAnimalAliveTime();
         GrassAnimalHungryTime();
+        
     }
 
-    void GrassAnimalAliveTime()
+    void GrassAnimalAliveTime() //草食動物 生命值
     {
         if(currentAlive > 0)
         {
@@ -53,11 +64,11 @@ public class GrassAnimal : MonoBehaviour
         }
     }
 
-    void GrassAnimalHungryTime()
+    void GrassAnimalHungryTime() //草食動物 飢餓值
     {
         hungryTime = maxHungry * 0.8f;
 
-        if(currentHungry > 0)
+        if(currentHungry > 0) 
         {
             currentHungry -= Time.deltaTime;
             hurgrySlider.value = currentHungry;
@@ -65,14 +76,103 @@ public class GrassAnimal : MonoBehaviour
 
         if (currentHungry < hungryTime)
         {
-            Debug.Log(gameObject.name + "想吃草了");
-
+            if(targetGrass == null)
+            {
+                Debug.Log(gameObject.name + "想吃草了");
+                FindGrass();
+            }
+           
         }
-        else if (currentHungry <= 0)
+        else
+        {
+            RandomMove();
+        }
+
+
+         if (currentHungry <= 0)
         {
             Debug.Log(gameObject.name + "餓死");
             Destroy(gameObject);
         }
+    }
+
+    void FindGrass()
+    {
+        Collider[] nearbyObject =Physics.OverlapSphere(transform.position,searchRadius); 
+        GameObject closestBigGrass = null;
+        GameObject closestSmallGrass = null;
+
+        foreach(Collider obj in nearbyObject)
+        {
+            if(obj.CompareTag("BigGrass"))
+            {
+                closestBigGrass = obj.gameObject;
+            }
+            else if(obj.CompareTag("SmallGrass") && closestBigGrass == null)
+            {
+                closestSmallGrass = obj.gameObject;
+            }
+        }
+
+
+        targetGrass = closestBigGrass != null ? closestBigGrass : closestSmallGrass;
+        if(targetGrass != null)
+        {
+            Debug.Log(gameObject.name +"找到草了~~");
+            MoveToTargetGrass();
+        }
+    }
+
+    void MoveToTargetGrass()
+    {
+        if (targetGrass != null)
+        {
+            Vector3 grassPos = targetGrass.transform.position;
+            agent.SetDestination(grassPos);
+
+
+            if (Vector3.Distance(transform.position, targetGrass.transform.position) < 0.5f)
+            {
+                Debug.Log(gameObject.name +"吃到草了!!");
+                targetGrass = null;
+            }
+        }
+    }
+
+    void RandomMove() //隨機遊蕩
+    {
+        isMoving = agent.velocity.magnitude > 0.1f;
+
+        if (!isMoving)
+        {
+            timer += Time.deltaTime;
+
+            if (timer >= waitTime)
+            {
+                Vector3 newPos = RandomLocation(moveRadius);
+                agent.SetDestination(newPos);
+                RandomWaitTime();
+                timer = 0;
+            }
+        }
+
+
+        Vector3 RandomLocation(float radius)
+        {
+
+            Vector3 randomDirection = Random.insideUnitSphere * radius;
+            randomDirection += transform.position;
+
+            NavMeshHit hit;
+            NavMesh.SamplePosition(randomDirection, out hit, radius, 1);
+            return hit.position;
+
+        }
+    }
+
+    void RandomWaitTime()
+    {
+        waitTime = Random.Range(1f, 5f);
     }
 
     void RandomLive()
